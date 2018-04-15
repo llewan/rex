77560400a2b34ed2f8cfe95ce03cff1f66890426
Leonardo Lewandowski <leonardo.lewandowski@mercadolibre.com>
2018-04-15 16:44:17 -0300

    Adding the library

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const EntitiesListener = () => {
+  console.log('EntitiesListener');
+};
+
+const EntitiesProvider = () => {
+  console.log('EntitiesProvider');
+};
+
+module.exports = {
+  EntitiesListener,
+  EntitiesProvider,
+};
